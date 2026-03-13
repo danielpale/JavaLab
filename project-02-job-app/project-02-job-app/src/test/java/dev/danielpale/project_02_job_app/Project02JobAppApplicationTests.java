a4be0a343780c930c32f0075d4e9060bbc322204
danielpale <danielpp79@gmail.com>
2026-03-12 18:23:00 -0600

    Project 02 - First commit

---
+++ project-02-job-app/project-02-job-app/src/test/java/dev/danielpale/project_02_job_app/Project02JobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danielpale.project_02_job_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project02JobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
